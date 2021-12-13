e4e975861783a65f5297bad18ad74da1b3f6cd0c
Claderoki <49477117+claderoki@users.noreply.github.com>
2021-12-13 09:32:02 +0100

    day5 finished

--- day5/__main__.py
+++ day5/__main__.py
@@ -5,6 +5,10 @@ path = __file__.replace('\\', '/').replace('__main__.py', '')
 with open(f'{path}/input') as f:
     data = f.read()
 
+def debug_rows(rows: list):
+    for row in rows:
+        print("".join([str(x) for x in row]))
+
 positions = []
 for line in data.splitlines():
     sides  = line.split(' -> ')
@@ -14,34 +18,72 @@ for line in data.splitlines():
     pos2 = Position(int(x2), int(y2))
     positions.append((pos1, pos2))
 
-SIZE = 10
-rows = [[Cell() for _ in range(SIZE)] for _ in range(SIZE)]
+def init_rows(size: int) -> list:
+    return [[Cell() for _ in range(size)] for _ in range(size)]
 
-def increment(start: Position, end: Position):
-    if start.x != 9:
-        return
+def __range(start, end) -> range:
+    if start > end:
+        step = -1
+        end -= 1
+    else:
+        step = 1
+        end += 1
 
-    print(start, end)
-    for i in range(start.x, end.x + 1, (-1 if start.x > end.x else 1)):
-        print(i, end = ', ')
-        rows[start.y][i].count += 1
-    print()
+    return range(start, end, step)
 
-    for i in range(start.y, end.y + 1, (-1 if start.y > end.y else 1)):
-        print(i, end = ', ')
-        rows[i][start.y].count += 1
-    print()
+def __increment_horizontally(rows: list, start: Position, end: Position):
+    row = start.y
+    for cell in __range(start.x, end.x):
+        rows[row][cell].count += 1
 
-for start, end in positions:
-    if start.x == end.x or start.y == end.y:
-        increment(start, end)
-        # break
+def __increment_vertically(rows: list, start: Position, end: Position):
+    cell = end.x
+    for row in __range(start.y, end.y):
+        rows[row][cell].count += 1
 
-for row in rows:
-    print("".join([str(x) for x in row]))
+def __increment_diagonally(rows: list, start: Position, end: Position):
+    y = list(__range(start.y, end.y))
+    for index, x in enumerate(__range(start.x, end.x)):
+        cell  = x
+        row = y[index]
+        rows[row][cell].count += 1
+
+def increment(rows: list, start: Position, end: Position):
+    if start.x == end.x:
+        __increment_vertically(rows, start, end)
+    elif start.y == end.y:
+        __increment_horizontally(rows, start, end)
+    else:
+        __increment_diagonally(rows, start, end)
+
+def count_overlap(rows):
+    overlap = 0
+    for row in rows:
+        for cell in row:
+            if cell.count > 1:
+                overlap += 1
+    return overlap
+
+SIZE = 999
 
 print('--- PART 1 (START) ---')
+
+rows = init_rows(SIZE)
+for start, end in positions:
+    if start.x != end.x and start.y != end.y:
+        continue
+    increment(rows, start, end)
+
+print(count_overlap(rows))
+
 print('--- PART 1 (END) ---')
 
 print('--- PART 2 (START) ---')
+
+rows = init_rows(SIZE)
+for start, end in positions:
+    increment(rows, start, end)
+
+print(count_overlap(rows))
+
 print('--- PART 2 (END) ---')
--- day5/helpers.py
+++ day5/helpers.py
@@ -12,7 +12,7 @@ class Cell(Base):
 
     def __init__(self):
         self.count = 0
-    
+
     def __str__(self) -> str:
         return str(self.count) if self.count > 0 else "."
 
@@ -22,11 +22,3 @@ class Position(Base):
     def __init__(self, x: int, y: int):
         self.x = x
         self.y = y
-
-    @classmethod
-    def from_raw(cls, raw) -> 'Position':
-        sides = raw.split(' -> ')
-        x1, y1 = sides[0].split(',')
-        x2, y2 = sides[1].split(',')
-        return cls(x1, y1)
-        print(raw)
